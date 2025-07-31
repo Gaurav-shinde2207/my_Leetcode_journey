d2a2aa75ba8baee1a33cd84e40b403b30fb52c8b
Gaurav Shashikant Shinde <109304723+Gaurav-shinde2207@users.noreply.github.com>
2025-08-01 00:13:27 +0530

    Time: 4 ms (5.71%) | Memory: 17.7 MB (55.16%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -2,10 +2,8 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         sol=[]
         for i in range(len(candies)):
-            if (candies[i]+extraCandies>= max(candies)):
-                sol.append(True)
-            else:
-                sol.append(False)
+            sol.append(candies[i]+extraCandies>= max(candies))
+        
 
         return sol
             
